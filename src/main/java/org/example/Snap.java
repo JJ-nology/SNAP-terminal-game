5be557f876cf8d973b7410ad1bf23c3cdbdc9834
JJ-blossom <73256107@protonmail.com>
2026-02-10 16:06:52 +0000

    Include player toggle method to Snap, update gameIsOn and checkCardSymbols to include 2 player functionality,

--- src/main/java/org/example/Snap.java
+++ src/main/java/org/example/Snap.java
@@ -12,6 +12,19 @@ public class Snap extends CardGame{
     protected Player currentPlayer;
     Scanner scanner = new Scanner(System.in);
 
+// welcome player to the game, give instructions, set up the game
+    public Snap() {
+        System.out.println("""
+                Welcome to SNAP!!\s
+                All you need to win the game is to draw 2 cards\s
+                in a row that have the same symbol i.e. 2 Jacks\s
+                This is a 2 player game so press ENTER when it's your turn.""");
+
+        setUpGame();
+        gameIsOn();
+
+    }
+
 // initial game set up
     private void setUpGame(){
         populateDeck();
@@ -25,6 +38,25 @@ public class Snap extends CardGame{
         currentPlayer = player1;
         System.out.printf( "%s press ENTER to begin.\n", currentPlayer.getName());
     }
+
+// run the game - While the player hasn't won, keep drawing cards
+    public void gameIsOn(){
+        scanner.nextLine();
+        dealCard();
+
+        currentCard = deckOfCards.getFirst();
+        if(previousCard == null) {
+            previousCard = currentCard;
+            removeCard();
+            togglePlayer();
+            System.out.printf("%s ,press ENTER to draw the next card", currentPlayer.getName());
+            gameIsOn();
+
+        }else {
+            checkCardSymbols();
+        }
+    }
+
 // set up player
     private Player playerSetUp(String playerName){
         if (playerName.isEmpty()) {
@@ -35,46 +67,31 @@ public class Snap extends CardGame{
         }
         return new Player(playerName);
     }
-// run the game - While the player hasn't won, keep drawing cards
-    public void gameIsOn(){
-        scanner.nextLine();
-        dealCard();
 
-        currentCard = deckOfCards.getFirst();
-        if(previousCard == null) {
-            previousCard = currentCard;
-            removeCard();
-            System.out.println("Press ENTER to draw the next card");
-            gameIsOn();
-
-        }else {
-            checkCardSymbols();
+// set up player toggle
+    private void togglePlayer(){
+        if (player1.getName().equals(currentPlayer.getName())) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
         }
+
     }
+
 // delete the card at the top of the deck
     public void removeCard(){
         deckOfCards.removeFirst();
     }
-// welcome player to the game, give instructions, set up the game
-    public Snap() {
-        System.out.println("""
-                Welcome to SNAP!!\s
-                All you need to win the game is to draw 2 cards\s
-                in a row that have the same symbol i.e. 2 Jacks\s
-                This is a 2 player game so press ENTER when it's your turn.""");
 
-        setUpGame();
-        gameIsOn();
-
-    }
     // check if current and previous cards has the same symbol. Continue game or display winner
     public void checkCardSymbols() {
         if (Objects.equals(currentCard.symbol, previousCard.symbol)) {
-            System.out.println("CONGRATULATIONS, YOU WIN");
+            System.out.println("CONGRATULATIONS, " + currentPlayer.getName() + " YOU WIN");
         }else {
             previousCard = currentCard;
             removeCard();
-            System.out.println("Press ENTER to draw again");
+            togglePlayer();
+            System.out.println(currentPlayer.getName() + " ,press ENTER to draw the next card");
             gameIsOn();
 
         }
